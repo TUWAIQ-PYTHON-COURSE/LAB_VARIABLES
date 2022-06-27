98fa7805cadc7d9cbbfb538083e41d13b198ab40
= <=>
2022-06-27 20:58:04 +0300

    solved lab

---
+++ MAIN.py
@@ -0,0 +1,16 @@
+
+#- Define a variable that has a value of your favourite football team.
+fav_football_team = "none"
+
+#- Define a variable “x“  has an integer value and a variable “y” has a float value.
+x : int = 3
+y : float = 23
+
+#- Assign One Value to Multiple Variables (choose random names & values).
+moon = sun = "amazing"
+
+#- Assign multiple Values to Multiple Variables (choose random names & values).
+p ,s = 3 ,6
+
+#- Use help to learn about yield
+help("yield")
