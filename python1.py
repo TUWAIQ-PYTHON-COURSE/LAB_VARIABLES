e88f1c01522f03e6d7ccc966f0cd24a5de6f09b5
saad <alrkhymys@gmail.com>
2022-06-27 20:19:21 +0300

    added python1.py a

---
+++ python1.py
@@ -0,0 +1,7 @@
+#Define a variable that has a value of your favourite football team.
+myFavouriteTeam = "alhilal"
+age : int = 20
+witht : float = 70.7
+a1 = a2 = a3 = "have same value"
+b1 , b2 , b3 = 11 , 12 , 13
+help("and")
