73f49796e750db9a4206d7a2373e9e8df5e96b06
Your Name <your_EMAIL@gmail.com>
2022-06-27 19:56:54 +0300

    lab2

---
+++ main.py
@@ -0,0 +1,19 @@
+favourite_football_team="Al Hilal"
+
+x : int =50
+y : float =30.5
+
+p1=p2=p3=40
+
+x1,x2,x3=20,30,"Al Hilal my favourite team"
+
+print("My favourite football team: "+favourite_football_team)
+print(x)
+print(y)
+print(p1)
+print(x1,x2)
+print(x3)
+'''The Yield keyword in Python is similar to a return statement used for returning values or objects in Python.'''
+print(type(x1)) 
+
+
