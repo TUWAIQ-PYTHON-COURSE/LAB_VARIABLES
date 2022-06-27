68978830c4e7cb9f48d9cf0689afcf1401160ba1
Yazeed Al-Harbi <yazeedal-harbi@Yazeeds-MacBook-Pro-4.local>
2022-06-27 19:58:58 +0300

    add variebls

---
+++ main_lab.py
@@ -0,0 +1,19 @@
+# define vaule of favorite team
+my_favorite_team = " ManUnited "
+print(my_favorite_team)
+
+# define variable X as Integer & Y as float 
+x = 7 
+print(x)
+y = 22.7
+print (y)
+
+# Assined One Value to Multiple Variables 
+f1 = t2 = r3 = 17 
+print (t2)
+
+# Assined One Value to Multiple Variables 
+c1, h2, s3, = 22, 15, 30 
+print (s3)
+
+help("yield")
