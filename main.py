a3c238689a03ab5713f10dff6414e4667c7cd4f6
unknown <hamadd2211@gmail.com>
2022-06-27 19:51:09 +0300

    Add new variables

---
+++ main.py
@@ -0,0 +1,9 @@
+favourite = "Alhilal"
+x : int = 10
+y : float = 3.14
+#Assign One Value to Multiple Variables
+a = b = 5
+#Assign multiple Values to Multiple Variables
+c,d = 3,2
+#Use help 
+help("yield")
