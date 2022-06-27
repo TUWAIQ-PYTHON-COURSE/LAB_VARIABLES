4190b93dbe7d2a6dd268df2ba75225cfe012bc19
fahad <fahadx1418@gmail.com>
2022-06-27 19:39:39 +0300

    done

---
+++ main.py
@@ -0,0 +1,36 @@
+
+#-Define a variable that has a value of your favourite football team.
+
+fivo_team = "My fivoret fitbool Team is : nasser"
+
+print(fivo_team)
+
+
+#- Define a variable “x“  has an integer value and a variable “y” has a float value.
+
+x : int = (11)
+
+y : float = (11.1337)
+
+print(x+y)
+
+
+#- Assign One Value to Multiple Variables (choose random names & values).
+
+a = b = c = ("its Value")
+print("var a "+a)
+print("var b "+b)
+print("var c "+c)
+
+#- Assign multiple Values to Multiple Variables (choose random names & values).
+
+a, b = "variables 1", "variables 2"
+
+print((""+a+b))
+
+
+#- Use help to learn about yield
+
+#
+
+
