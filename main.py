812a1cdfe720c7cf73efedaaf76fede64bcfa977
Yahya albariqi <kaizik.y18@gmail.com>
2022-06-27 20:01:48 +0300

    Lab completed main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 
+from pydoc import Helper
+
+
 some_var = "Al-lttihad"
 
 some_var1 : int = 40
@@ -9,9 +12,5 @@ y1 = y2 = x5 = "Yahya Love python"
 
 m1, m2 = 360, 250
 
-print(some_var)
-print(some_var1)
-print(some_var2)
-print(y2)
-print(m2)
-print(m1)
+
+help('yield')
