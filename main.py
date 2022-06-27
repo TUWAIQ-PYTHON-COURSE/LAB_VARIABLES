528d0bf3d03f1df6fe4498a0fc20f69a69114b4e
Yahya albariqi <kaizik.y18@gmail.com>
2022-06-27 19:40:20 +0300

    Lab completed main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+some_var = "Al-lttihad"
+
+some_var1 : int = 40
+some_var2 : float = 5.5
+
+y1 = y2 = x5 = "Yahya Love python"
+
+m1, m2 = 360, 250
+
+print(some_var)
+print(some_var1)
+print(some_var2)
+print(y2)
+print(m2)
+print(m1)
