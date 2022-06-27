13131f837f3cfffc52d0a52a41c7194f1453fb79
hashem-ali <hashem.bajabaa1@gmail.com>
2022-06-27 20:07:08 +0300

    using help func

--- main.py
+++ main.py
@@ -7,21 +7,23 @@ x1 = x2 = x3 = 10
 print(x1, x2, x3)
 var1, var2 = 5, 8
 print(var1, var2)
+
+help("yield")
 # print(yield(my_team))
-def generator():
+# def generator():
 
-   yield "Welcome"
+#    yield "Welcome"
 
-   yield "to"
+#    yield "to"
 
-   yield "Simplilearn"
-# this is a generator object
-gen_object = generator()
+#    yield "Simplilearn"
+# # this is a generator object
+# gen_object = generator()
 
-print(type(gen_object))
-# next is a func that 
-# return the next element for object
+# print(type(gen_object))
+# # next is a func that 
+# # return the next element for object
 
-print(next(gen_object))
-print(next(gen_object))
-print(next(gen_object))
+# print(next(gen_object))
+# print(next(gen_object))
+# print(next(gen_object))
