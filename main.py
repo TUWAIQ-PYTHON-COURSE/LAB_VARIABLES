02169213d539a54c129ffa1384cbd7d353ed7182
mohammed marroti <m7md.marroti@gmail.com>
2022-06-27 20:13:17 +0300

    finsh lap

---
+++ main.py
@@ -0,0 +1,20 @@
+#- Define a variable that has a value of your favourite football team.
+my_favourite_team= "al_hilal" 
+
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+x :int = 10  
+y :float = 13.5  
+
+# Assign One Value to Multiple Variables (choose random names & values).
+c1= c2= c3= "Hello world"  
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+a1, a2= 4, 5
+
+print(my_favourite_team)
+print(type(x))
+print(type(y))
+print(c1,c2,c3)
+print(a1,a2)
+help("yield")
+
