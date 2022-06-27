4ef89109ba90d774e336c645e2fc6da7d5f21bfa
nora <nora_abdullah33@outlook.com>
2022-06-27 20:09:19 +0300

    My HomeWork

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import numbers
+
+
+footBall_Team : str = "AL-Hallal" 
+X : int = 4 
+Y : float = 5.03 
+var1 = var2 = var3 = "All have the same value" 
+
+Z1 , Z2 = 2022 , 2021 
+
+help("yield")
+
+
