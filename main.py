4235328e8e950fafac8ac73b622e148acde2b246
Mashael S <mashaeelsaaud94@gmail.com>
2022-06-27 20:37:01 +0300

    lab2

--- main.py
+++ main.py
@@ -3,14 +3,9 @@ my_favourite_team = "AlNassr"
 var_x : int = 40
 var_y : float = 20.5
 
-x1 = x2 = z3 = "Mashael AlNassr"
+x1 = x2 = z3 = 5
 
 y1, y2 = 100, 5000
 
 
-print(my_favourite_team)
-print(var_x)
-print(var_y)
-print(x2)
-print(y2)
-print(y1)
+help("yield")
