68d198e9a52838452747e0abb9617dbcec0222db
Mashael S <mashaeelsaaud94@gmail.com>
2022-06-27 19:57:03 +0300

    lab2

---
+++ main.py
@@ -0,0 +1,16 @@
+my_favourite_team = "AlNassr"
+
+var_x : int = 40
+var_y : float = 20.5
+
+x1 = x2 = z3 = "Mashael AlNassr"
+
+y1, y2 = 100, 5000
+
+
+print(my_favourite_team)
+print(var_x)
+print(var_y)
+print(x2)
+print(y2)
+print(y1)
